b62acd7ac93a3ce4afb2ba1201dcc87424076ba8
Pedro Teixeira <pedro.teixeira@gmail.com>
2010-09-27 16:10:05 +0100

    first commit

---
+++ example_app/test_app.js
@@ -0,0 +1,18 @@
+var fugue = require('../lib/fugue.js'),
+    net =   require('net');
+
+var server = net.createServer(function(conn) {
+  conn.end(process.pid.toString() + "Hello from "+fugue.workerId()+". I am user " + process.getuid() + " on dir " + process.cwd());
+});
+
+fugue.start(server, 4000, null, 2, {
+//fugue.start(server, 'tmp/my_test_server.sock', 2, {
+  daemonize: true,
+  // log_file: process.cwd() +'/log/children.txt',
+  // master_log_file: process.cwd() +'/log/master.txt',
+  uid: 'pedroteixeira',
+  gid: 'staff',
+  working_path: '/tmp',
+  tmp_path: process.cwd() + '/tmp',
+  verbose: true
+});
