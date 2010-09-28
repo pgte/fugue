4574991516e8d04ecefde1b58b1bfb6d724bc365
Pedro Teixeira <pedro.teixeira@gmail.com>
2010-09-28 15:03:25 +0100

    fixed fugue shutdown. playing with daemonizing

--- example_app/test_app.js
+++ example_app/test_app.js
@@ -1,4 +1,8 @@
-var fugue = require('fugue'),
+process.on('uncaughtException', function(excp) {
+  console.log(require('sys').inspect(excp));
+});
+
+var fugue = require('../lib/fugue.js'),
     net =   require('net');
 
 var server = net.createServer(function(conn) {
@@ -7,7 +11,7 @@ var server = net.createServer(function(conn) {
 
 fugue.start(server, 4000, null, 2, {
 //fugue.start(server, 'tmp/my_test_server.sock', 2, {
-  daemonize: true,
+  daemonize: false,
   // log_file: process.cwd() +'/log/children.txt',
   // master_log_file: process.cwd() +'/log/master.txt',
   uid: 'pedroteixeira',
@@ -16,3 +20,6 @@ fugue.start(server, 4000, null, 2, {
   tmp_path: process.cwd() + '/tmp',
   verbose: true
 });
+
+console.log('done here');
+
