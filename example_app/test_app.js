428442c820dd88f96cf223a53d1c2c95d810e828
Pedro Teixeira <pedro.teixeira@gmail.com>
2010-09-28 16:46:51 +0100

    removing socket file before binding

--- example_app/test_app.js
+++ example_app/test_app.js
@@ -10,10 +10,10 @@ var server = net.createServer(function(conn) {
   conn.end(process.pid.toString() + "Hello from "+fugue.workerId()+". I am user " + process.getuid() + " on dir " + process.cwd());
 });
 
-fugue.start(server, 4000, null, 2, {
-//fugue.start(server, 'tmp/my_test_server.sock', 2, {
-  daemonize: true,
-  log_file: process.cwd() +'/log/children.txt',
+//fugue.start(server, 4000, null, 2, {
+fugue.start(server, 'tmp/my_test_server.sock', 2, {
+  daemonize: false,
+  //log_file: process.cwd() +'/log/children.txt',
   //master_log_file: process.cwd() +'/log/master.txt',
   uid: 'pedroteixeira',
   gid: 'staff',
