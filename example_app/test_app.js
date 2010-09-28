4187bf6dd5e9b58c2bed06642f7fd7406d23c45c
Pedro Teixeira <pedro.teixeira@gmail.com>
2010-09-28 22:23:14 +0100

    worked on the todo list

--- example_app/test_app.js
+++ example_app/test_app.js
@@ -12,7 +12,7 @@ var server = net.createServer(function(conn) {
 
 //fugue.start(server, 4000, null, 2, {
 fugue.start(server, 'tmp/my_test_server.sock', 2, {
-  daemonize: false,
+  daemonize: true,
   //log_file: process.cwd() +'/log/children.txt',
   //master_log_file: process.cwd() +'/log/master.txt',
   uid: 'pedroteixeira',
