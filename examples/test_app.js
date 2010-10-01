72a682f166373d2dfae2f3b6b1ce47beb4417bd1
Pedro Teixeira <pedro.teixeira@gmail.com>
2010-10-01 12:03:02 +0100

    Release v0.0.14

--- examples/test_app.js
+++ examples/test_app.js
@@ -9,7 +9,7 @@ var fugue = require('../lib/fugue.js'),
 
 var server = net.createServer(function(conn) {
   //setTimeout(function() {
-    conn.end(process.pid.toString() + "Hello from "+fugue.workerId()+". I am user " + process.getuid() + " on dir " + process.cwd());
+    conn.end("Hello from worker "+fugue.workerId()+".\nI am user " + process.getuid() + "\non dir " + process.cwd()+"\nPID: "+process.pid.toString() + "\n");
   //}, 5000);
   
 });
@@ -17,8 +17,8 @@ var server = net.createServer(function(conn) {
 fugue.start(server, 4000, null, 2, {
 //fugue.start(server, 'tmp/my_test_server.sock', 2, {
   daemonize: false,
-  //log_file: process.cwd() +'/log/children.txt',
-  //master_log_file: process.cwd() +'/log/master.txt',
+  log_file: process.cwd() +'/log/children.txt',
+  master_log_file: process.cwd() +'/log/master.txt',
   // uid: 'pedroteixeira',
   // gid: 'staff',
   working_path: '/tmp',
